2bf3be497d6e73dab4c11bd139ef08f1f79535ce
DavideIsaia <davide.enjoy@hotmail.it>
2022-05-06 14:39:46 +0200

    aggiunta funzionalità tasti prev e next

--- js/app.js
+++ js/app.js
@@ -1,85 +1,70 @@
+// Descrizione:
+// Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
+// Bonus:
+// 1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+// 2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// 3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce
+
 Vue.config.devtools = true;
 const app = new Vue (
     {
         el: '#root',
         data: {
-            images : [
-                'img/01.jpg',
-                'img/02.jpg',
-                'img/03.jpg',
-                'img/04.jpg',
-                'img/05.jpg'
+            slides: [
+                {
+                    image:'img/01.jpg',
+                    title:'Svezia',
+                    text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+                },
+                {
+                    image:'img/02.jpg',
+                    title:'Svizzera',
+                    text:'Lorem ipsum'
+                },
+                {
+                    image:'img/03.jpg',
+                    title:'Gran Bretagna',
+                    text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+                },
+                {
+                    image:'img/04.jpg',
+                    title:'Germania',
+                    text:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+                },
+                {
+                    image:'img/05.jpg',
+                    title:'Paradise',
+                    text:'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+                }                
             ],
-            
-            title: [
-                'Svezia',
-                'Svizzera',
-                'Gran Bretagna',
-                'Germania',
-                'Paradise'
-            ],
-            
-            text : [
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-                'Lorem ipsum',
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-                'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-                'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-            ]            
+            currentSlide: 0                                
         },
-        methods: {
 
+        methods: {
+            // Slide precedente
+            prevSlide(){
+                this.currentSlide --; // precedente
+
+                // se sono alla prima, mi posiziono sull'ultima
+                if(this.currentSlide < 0){
+                    this.currentSlide = this.slides.length - 1; // ultima
+                }
+                // console.log('Prev', this.currentSlide);
+            },
+
+            // Slide successiva
+            nextSlide(){
+                this.currentSlide ++; // successiva
+
+                // se sono all'ultima, mi posiziono sulla prima
+                if(this.currentSlide > this.slides.length - 1){
+                    this.currentSlide = 0;
+                }
+                // console.log('Next', this.currentSlide);
+            }
         }
     }
 )
-
-// // 1. Uniamo i 3 array di sopra in un array di oggetti
-// const data = [];
-
-// for (let i = 0; i < images.length; i++) {
-//     data.push({
-//         image: images[i],
-//         title: title[i],
-//         text : text[i]
-//     });
-// }
-
-// console.log(data);
-
-// const itemsContainer  = document.querySelector('.items');
-// const thumbsContainer = document.getElementsByClassName('thumbs')[0]; // document.querySelector('.thumbs')
-// let currentSlide      = 0; // ci posizioniamo sulla prima slide
-
-// createItems();
-// createThumbnails();
-
-// // Slide precedente
-// document.querySelector('.prev').addEventListener('click', function() {
-
-//     // se sono alla prima, mi posiziono sull'ultima
-//     if (currentSlide == 0) {
-//         currentSlide = data.length - 1; // ultima
-//     } else {
-//         currentSlide--; // precedente
-//     }
-
-//     console.log('Prev', currentSlide);
-//     changeActive(currentSlide);
-// });
-
-// // Slide successiva
-// document.querySelector('.next').addEventListener('click', function() {
-
-//     // se sono all'ultima, mi posiziono sulla prima
-//     if (currentSlide == data.length - 1) {
-//         currentSlide = 0; // prima
-//     } else {
-//         currentSlide++; // successiva
-//     }
-
-//     console.log('Next', currentSlide);
-//     changeActive(currentSlide);
-// });
 
 // function changeActive(index) {
 
